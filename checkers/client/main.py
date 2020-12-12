ae150a3dcf99d5e9506efaa0e53d267419f40443
Amit_Games <amitfridman78@gmail.com>
2020-12-12 15:51:24 +0200

    changed code and added screen.py

--- checkers/client/main.py
+++ checkers/client/main.py
@@ -1,13 +1,17 @@
 from socket import socket
 import pygame
 
+from checkers.client.screen import screen
+
 HOST = 'localhost'
 PORT = 5555
 WIDTH = 1280
 HEIGHT = 720
-
+running = True
 
 def main():
+    s = screen(WIDTH, HEIGHT)
+
     with socket() as client:
         try:
             client.connect((HOST, PORT))
@@ -16,6 +20,10 @@ def main():
         print(client.recv(1024).decode())
         choice = input("What Would you like to do")
         client.send("IDK".encode())
+        while running:
+            running = s.check_quit()
+
+
 
 
 if __name__ == '__main__':
--- checkers/client/screen.py
+++ checkers/client/screen.py
@@ -1,6 +1,17 @@
 import pygame
+import sys
+
 
 class screen:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.init()
+
+    @staticmethod
+    def check_quit(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+        return True
